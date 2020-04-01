9d71a368c64c079f3a04ba03d2ee013e04889b4b
aishwarya1112 <58583506+aishwarya1112@users.noreply.github.com>
2020-04-01 13:43:21 +0530

    Update ml4_python.py

--- ml4_python.py
+++ ml4_python.py
@@ -4,15 +4,15 @@ ANS_LIST = ["apple", "banana", "grapes", "watermelon", "strawberry"]
 random.shuffle(ANS_LIST)
 Answer = list(ANS_LIST[0])
 
-Disp = []
+DISP = []
 USED = []
-Disp.extend(Answer)
-USED.extend(Disp)
+DISP.extend(Answer)
+USED.extend(DISP)
 
-for i in range(len(Disp)):
+for i in range(len(DISP)):
     Disp[i] = "_"
 
-print(' '.join(Disp))
+print(' '.join(DISP))
 print()
 Count = 0
 
@@ -23,18 +23,18 @@ while Count < len(Answer):
 
     for i in range(len(Answer)):
         if Answer[i] == GUESS and GUESS in USED:
-            Disp[i] = GUESS
+            DISP[i] = GUESS
             Count = Count + 1
             USED.remove(GUESS)
 
 
-    if GUESS not in Disp:
+    if GUESS not in DISP:
         print("sorry,wrong guess")
 
 
 
     print("You have guessed: ", COUNT, "correct letters.")
-    print(' '.join(Disp))
+    print(' '.join(DISP))
     print()
 
 print("well done, you guessed the fruit")
